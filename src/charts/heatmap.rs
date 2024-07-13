ff7906e6d32795cabb8e78584e97e2ddc735b16f
Berke <63060680+akenshaw@users.noreply.github.com>
2024-07-13 19:08:35 +0300

    carry over x-axis label calc. to super

--- src/charts/heatmap.rs
+++ src/charts/heatmap.rs
@@ -6,7 +6,7 @@ use iced::{
 use iced::widget::{Column, Row, Container, Text};
 use crate::data_providers::binance::market_data::{LocalDepthCache, Trade};
 
-use super::{Chart, CommonChartData, Message, chart_button, calculate_price_step, Interaction, AxisLabelYCanvas};
+use super::{Chart, CommonChartData, Message, chart_button, Interaction, AxisLabelYCanvas, AxisLabelXCanvas};
 
 pub struct HeatmapChart {
     chart: CommonChartData,
@@ -198,6 +198,7 @@ impl HeatmapChart {
                 crosshair_cache: &chart_state.x_crosshair_cache, 
                 crosshair_position: chart_state.crosshair_position, 
                 crosshair: chart_state.crosshair,
+                timeframe: None,
             })
             .width(Length::FillPortion(10))
             .height(Length::Fixed(26.0));
@@ -673,159 +674,6 @@ impl canvas::Program<Message> for HeatmapChart {
                 }
             }
             Interaction::None => { mouse::Interaction::default() }
-        }
-    }
-}
-
-const TIME_STEPS: [i64; 8] = [
-    60 * 1000, // 1 minute
-    30 * 1000, // 30 seconds
-    15 * 1000, // 15 seconds
-    10 * 1000, // 10 seconds
-    5 * 1000,  // 5 seconds
-    2 * 1000,  // 2 seconds
-    1000,  // 1 second
-    500,       // 500 milliseconds
-];
-fn calculate_time_step(earliest: i64, latest: i64, labels_can_fit: i32) -> (i64, i64) {
-    let duration = latest - earliest;
-
-    let mut selected_step = TIME_STEPS[0];
-    for &step in &TIME_STEPS {
-        if duration / step >= labels_can_fit as i64 {
-            selected_step = step;
-            break;
-        }
-    }
-
-    let rounded_earliest = (earliest / selected_step) * selected_step;
-
-    (selected_step, rounded_earliest)
-}
-
-pub struct AxisLabelXCanvas<'a> {
-    labels_cache: &'a Cache,
-    crosshair_cache: &'a Cache,
-    crosshair_position: Point,
-    crosshair: bool,
-    min: i64,
-    max: i64,
-}
-impl canvas::Program<Message> for AxisLabelXCanvas<'_> {
-    type State = Interaction;
-
-    fn update(
-        &self,
-        _interaction: &mut Interaction,
-        _event: Event,
-        _bounds: Rectangle,
-        _cursor: mouse::Cursor,
-    ) -> (event::Status, Option<Message>) {
-        (event::Status::Ignored, None)
-    }
-    
-    fn draw(
-        &self,
-        _state: &Self::State,
-        renderer: &Renderer,
-        _theme: &Theme,
-        bounds: Rectangle,
-        _cursor: mouse::Cursor,
-    ) -> Vec<Geometry> {
-        if self.max == 0 {
-            return vec![];
-        }
-        let latest_in_millis = self.max; 
-        let earliest_in_millis = self.min; 
-
-        let x_labels_can_fit = (bounds.width / 120.0) as i32;
-        let (time_step, rounded_earliest) = calculate_time_step(self.min, self.max, x_labels_can_fit);
-
-        let labels = self.labels_cache.draw(renderer, bounds.size(), |frame| {
-            frame.with_save(|frame| {
-                let mut time: i64 = rounded_earliest;
-                let latest_time: i64 = latest_in_millis;
-
-                while time <= latest_time {                    
-                    let x_position = ((time - earliest_in_millis) as f64 / (latest_in_millis - earliest_in_millis) as f64) * bounds.width as f64;
-
-                    if x_position >= 0.0 && x_position <= bounds.width as f64 {
-                        let text_size = 12.0;
-                        let time_as_datetime = NaiveDateTime::from_timestamp(time / 1000, 0);
-                        let label = canvas::Text {
-                            content: time_as_datetime.format("%M:%S").to_string(),
-                            position: Point::new(x_position as f32 - text_size, bounds.height - 20.0),
-                            size: iced::Pixels(text_size),
-                            color: Color::from_rgba8(200, 200, 200, 1.0),
-                            ..canvas::Text::default()
-                        };  
-
-                        label.draw_with(|path, color| {
-                            frame.fill(&path, color);
-                        });
-                    }
-                    
-                    time += time_step;
-                }
-
-                let line = Path::line(
-                    Point::new(0.0, bounds.height - 30.0), 
-                    Point::new(bounds.width, bounds.height - 30.0)
-                );
-                frame.stroke(&line, Stroke::default().with_color(Color::from_rgba8(81, 81, 81, 0.2)).with_width(1.0));
-            });
-        });
-        let crosshair = self.crosshair_cache.draw(renderer, bounds.size(), |frame| {
-            if self.crosshair && self.crosshair_position.x > 0.0 {
-                let crosshair_ratio = self.crosshair_position.x as f64 / bounds.width as f64;
-                let crosshair_millis = (earliest_in_millis as f64 + crosshair_ratio * (latest_in_millis as f64 - earliest_in_millis as f64)).round() / 100.0 * 100.0;
-                let crosshair_time = NaiveDateTime::from_timestamp((crosshair_millis / 1000.0).floor() as i64, ((crosshair_millis % 1000.0) * 1_000_000.0).round() as u32);
-                
-                let crosshair_timestamp = crosshair_time.timestamp_millis();
-
-                let snap_ratio = (crosshair_timestamp as f64 - earliest_in_millis as f64) / (latest_in_millis as f64 - earliest_in_millis as f64);
-                let snap_x = snap_ratio * bounds.width as f64;
-
-                let text_size = 12.0;
-                let text_content = crosshair_time.format("%M:%S:%3f").to_string().replace('.', "");
-                let growth_amount = 6.0; 
-                let rectangle_position = Point::new(snap_x as f32 - 26.0 - growth_amount, bounds.height - 20.0);
-                let text_position = Point::new(snap_x as f32 - 26.0, bounds.height - 20.0);
-
-                let text_background = canvas::Path::rectangle(rectangle_position, Size::new(text_content.len() as f32 * text_size/2.0 + 2.0 * growth_amount, text_size + text_size/2.0));
-                frame.fill(&text_background, Color::from_rgba8(200, 200, 200, 1.0));
-
-                let crosshair_label = canvas::Text {
-                    content: text_content,
-                    position: text_position,
-                    size: iced::Pixels(text_size),
-                    color: Color::from_rgba8(0, 0, 0, 1.0),
-                    ..canvas::Text::default()
-                };
-
-                crosshair_label.draw_with(|path, color| {
-                    frame.fill(&path, color);
-                });
-            }
-        });
-
-        vec![labels, crosshair]
-    }
-
-    fn mouse_interaction(
-        &self,
-        interaction: &Interaction,
-        bounds: Rectangle,
-        cursor: mouse::Cursor,
-    ) -> mouse::Interaction {
-        match interaction {
-            Interaction::Drawing => mouse::Interaction::Crosshair,
-            Interaction::Erasing => mouse::Interaction::Crosshair,
-            Interaction::Panning { .. } => mouse::Interaction::ResizingHorizontally,
-            Interaction::None if cursor.is_over(bounds) => {
-                mouse::Interaction::ResizingHorizontally
-            }
-            Interaction::None => mouse::Interaction::default(),
         }
     }
 }
